bf838482318b1ff7f1c9d2b5494f8522924da820
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2024-09-26 08:17:32 -0600

    feat(Migraciones): Se crearon las migraciones de las tablas "Sedes", "Facultades" y "Aulas".

---
+++ app/Models/Sedes.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sedes extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sedes';
+    protected $fillable = ['nombre', 'direccion', 'activo'];
+}
