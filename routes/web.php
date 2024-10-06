624b53163650119f11fb88912e7e048c8fb8b628
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2024-10-05 18:13:06 -0600

    feat(): Maquetación del listado de reportes general iniciada.

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/reportes-generales', function () {
+    return view('listado-reportes.reportes_generales');
+});
+
+require __DIR__ . '/auth.php';
