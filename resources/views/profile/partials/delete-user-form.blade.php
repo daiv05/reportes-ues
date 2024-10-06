7fad5076eb68f08c718247134c5eb465ec0bc98a
David DV <71719478+daiv05@users.noreply.github.com>
2024-10-05 18:15:00 -0600

    vista creacion de reporte v1

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -10,7 +10,7 @@
     </header>
 
     <x-danger-button x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')">
-        {{ __('Delete Account') }}
+        {{ __('Eliminar cuenta') }}
     </x-danger-button>
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
