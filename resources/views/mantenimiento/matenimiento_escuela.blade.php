9cf1ac4d042898d3d07c8704c3fce0f8cd1066cd
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2024-10-07 17:04:14 -0600

    usar componentes

--- resources/views/mantenimiento/matenimiento_escuela.blade.php
+++ resources/views/mantenimiento/matenimiento_escuela.blade.php
@@ -176,98 +176,82 @@
                                             }
                                         </script>
                                         <!-- Modal de Edición -->
-                                        <div id="editModal{{ $escuela->id }}" tabindex="-1" aria-hidden="true"
-                                            class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto max-h-full">
-                                            <div class="relative w-full max-w-2xl max-h-full">
-                                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                                    <!-- Modal header -->
-                                                    <div
-                                                        class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                                            Editar Escuela
-                                                        </h3>
-                                                        <button type="button"
-                                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                            data-modal-hide="editModal{{ $escuela->id }}">
-                                                            <svg aria-hidden="true" class="w-5 h-5"
-                                                                fill="currentColor" viewBox="0 0 20 20"
-                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                <path fill-rule="evenodd"
-                                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                                    clip-rule="evenodd"></path>
-                                                            </svg>
-                                                            <span class="sr-only">Cerrar modal</span>
-                                                        </button>
+                                        <x-form-modal :id="'editModal' . $escuela->id">
+                                            <x-slot name="header">
+                                                Editar Escuela
+                                                <button type="button"
+                                                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    data-modal-hide="editModal{{ $escuela->id }}">
+                                                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
+                                                        viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                        <path fill-rule="evenodd"
+                                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 011.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                            clip-rule="evenodd"></path>
+                                                    </svg>
+                                                    <span class="sr-only">Cerrar modal</span>
+                                                </button>
+                                            </x-slot>
+
+                                            <x-slot name="body">
+                                                <form action="{{ route('escuela.update', $escuela->id) }}"
+                                                    method="POST">
+                                                    @csrf
+                                                    @method('PUT')
+
+                                                    <!-- Facultad ID (Select con lista de facultades) -->
+                                                    <div class="w-full">
+                                                        <label for="id_facultad"
+                                                            class="block mb-2 text-sm font-medium text-gray-900">Facultad</label>
+                                                        <select id="id_facultad" name="id_facultad" required
+                                                            class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                            <option value="1"
+                                                                {{ $escuela->id_facultad == 1 ? 'selected' : '' }}>
+                                                                Ingeniería</option>
+                                                            <option value="2"
+                                                                {{ $escuela->id_facultad == 2 ? 'selected' : '' }}>
+                                                                Arquitectura</option>
+                                                        </select>
                                                     </div>
 
-                                                    <!-- Modal body -->
-
-                                                    <div class="p-6 space-y-6">
-                                                        <form action="{{ route('escuela.update', $escuela->id) }}"
-                                                            method="POST">
-                                                            @csrf
-                                                            @method('PUT')
-
-                                                            <!-- Facultad ID (Select con lista de facultades) -->
-                                                            <div class="w-full">
-                                                                <label for="id_facultad"
-                                                                    class="block mb-2 text-sm font-medium text-gray-900">Facultad</label>
-                                                                <select id="id_facultad" name="id_facultad" required
-                                                                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                                                    <option value="1"
-                                                                        {{ $escuela->id_facultad == 1 ? 'selected' : '' }}>
-                                                                        Ingeniería</option>
-                                                                    <option value="2"
-                                                                        {{ $escuela->id_facultad == 2 ? 'selected' : '' }}>
-                                                                        Arquitectura</option>
-                                                                </select>
-                                                                </select>
-                                                            </div>
-                                                            <!-- Nombre de la Escuela -->
-                                                            <div class="mb-4">
-                                                                <label for="nombre"
-                                                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nombre
-                                                                    de la
-                                                                    Escuela</label>
-                                                                <input type="text" name="nombre" id="nombre"
-                                                                    value="{{ $escuela->nombre }}" required
-                                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                                    placeholder="Ingrese el nombre de la escuela">
-                                                            </div>
-
-                                                            <!-- Estado de la Escuela -->
-                                                            <div class="mb-4">
-                                                                <label for="activo"
-                                                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Estado</label>
-                                                                <select name="activo" id="activo"
-                                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                                                    <option value="1"
-                                                                        {{ $escuela->activo ? 'selected' : '' }}>Activo
-                                                                    </option>
-                                                                    <option value="0"
-                                                                        {{ !$escuela->activo ? 'selected' : '' }}>
-                                                                        Inactivo</option>
-                                                                </select>
-                                                            </div>
-
-                                                            <!-- Modal footer -->
-                                                            <div
-                                                                class="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                                                <button data-modal-hide="editModal{{ $escuela->id }}"
-                                                                    type="submit"
-                                                                    class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Guardar
-                                                                    cambios</button>
-                                                                <button data-modal-hide="editModal{{ $escuela->id }}"
-                                                                    type="button"
-                                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">Cancelar</button>
-                                                            </div>
-                                                        </form>
+                                                    <!-- Nombre de la Escuela -->
+                                                    <div class="mb-4">
+                                                        <label for="nombre"
+                                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Nombre
+                                                            de la
+                                                            Escuela</label>
+                                                        <input type="text" name="nombre" id="nombre"
+                                                            value="{{ $escuela->nombre }}" required
+                                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                                            placeholder="Ingrese el nombre de la escuela">
                                                     </div>
 
+                                                    <!-- Estado de la Escuela -->
+                                                    <div class="mb-4">
+                                                        <label for="activo"
+                                                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Estado</label>
+                                                        <select name="activo" id="activo"
+                                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                            <option value="1"
+                                                                {{ $escuela->activo ? 'selected' : '' }}>Activo
+                                                            </option>
+                                                            <option value="0"
+                                                                {{ !$escuela->activo ? 'selected' : '' }}>Inactivo
+                                                            </option>
+                                                        </select>
+                                                    </div>
+                                            </x-slot>
 
-                                                </div>
-                                            </div>
-                                        </div>
+                                            <x-slot name="footer">
+                                                <button data-modal-hide="editModal{{ $escuela->id }}" type="submit"
+                                                    class="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                                    Guardar cambios
+                                                </button>
+                                                <button data-modal-hide="editModal{{ $escuela->id }}" type="button"
+                                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
+                                                    Cancelar
+                                                </button>
+                                            </x-slot>
+                                        </x-form-modal>
                                     @endforeach
                                 </tbody>
                             </table>
