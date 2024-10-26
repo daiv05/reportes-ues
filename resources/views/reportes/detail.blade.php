271f3161aabfd692fa3b3ebf73cacd19de3ef45b
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2024-10-25 21:30:24 -0600

    feat(Reportes/Listado): Inicio del consumo de los endpoint

--- resources/views/reportes/detail.blade.php
+++ resources/views/reportes/detail.blade.php
@@ -86,5 +86,123 @@
                 </div>
             </div>
         </div>
+
+        <div class="flex flex-col lg:flex-row w-full mt-8">
+            <!-- Columna izquierda (70%) -->
+            <div class="w-full lg:w-[60%] px-8">
+                <div class="mb-4">
+                    <!-- Fila 1 -->
+                    <div class="text-xl font-bold ml-12 mb-8">
+                        <p>Asignación</p>
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <!-- Fila 2 -->
+                    <div class="font-semibold">
+                        <div class="flex flex-row gap-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z"/>
+                            </svg>
+                            Entidad
+                        </div>
+                        <div class="ml-12 mt-2">
+                            <select id="entidad"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">
+                                @foreach($entidades as $entidad)
+                                    <option
+                                        value="{{ $entidad->id }}" {{ $reporte->id_entidad == $entidad->id ? 'selected' : '' }}>{{ $entidad->nombre }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <!-- Fila 3 -->
+                    <div class="font-semibold">
+                        <div class="flex flex-row gap-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
+                            </svg>
+                            Asignado a
+
+                            <button
+                                class="bg-white text-black border border-black px-5 rounded">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2 -2 30 30"
+                                     stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="ml-12 mt-2">
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <!-- Fila 3 -->
+                    <div class="font-semibold">
+                        <div class="flex flex-row gap-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"/>
+                            </svg>
+                            Supervisor
+
+                            <button
+                                class="bg-white text-black border border-black px-5 ml-0.5 rounded">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-2 -2 30 30"
+                                     stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div class="ml-12 mt-2">
+
+                        </div>
+
+                    </div>
+                </div>
+                <div class="mb-4">
+                    <!-- Fila 2 -->
+                    <div class="font-semibold">
+                        <div class="flex flex-row gap-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"/>
+                            </svg>
+                            Comentario de administración
+                        </div>
+                        <div class="ml-12 mt-2">
+                            <x-text-area id="comentario" rows="8" :disabled="false">
+                            </x-text-area>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <!-- Columna derecha (30%) -->
+            <div class="w-full lg:w-[25%]">
+                <button class="bg-red-700 text-white text-sm py-2 px-4 rounded hover:bg-red-500">
+                    Marcar como No procede
+                </button>
+            </div>
+        </div>
+
+        <div class="flex flex-col lg:flex-row w-full justify-center mt-8">
+            <button class="bg-escarlata-ues text-white text-sm py-2 px-4 rounded hover:bg-red-700">
+                Enviar Asignación
+            </button>
+        </div>
     </div>
 </x-app-layout>
