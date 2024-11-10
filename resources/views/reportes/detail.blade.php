64042b924774c4c9bf94531fec9b0bd9ef0c8db6
David DV <71719478+daiv05@users.noreply.github.com>
2024-11-09 18:08:37 -0600

    se agrega a principal

--- resources/views/reportes/detail.blade.php
+++ resources/views/reportes/detail.blade.php
@@ -87,9 +87,9 @@
             </div>
         </div>
 
-        <x-general.divider/>
-
         @if((!$reporte->estado_ultimo_historial?->nombre) && $reporte->no_procede == 0)
+        
+        <x-general.divider/>
             <form method="POST"
                   action="{{ route('reportes.realizarAsignacion', ['id' => $reporte->id]) }}"
                   enctype="multipart/form-data">
@@ -616,6 +616,72 @@
             <!-- Columna izquierda (80%) -->
             <div class="w-full lg:w-[80%] px-8">
                 {{--                Espacio para el timeline--}}
+                <div class="mt-4"></div>
+                <div class="mb-4">
+                    <!-- Fila 1 -->
+                    <div class="text-xl font-bold ml-12 mb-8">
+                        <p>Historial</p>
+                    </div>
+                </div>
+                @foreach ($reporte->accionesReporte->historialAccionesReporte as $historial)
+                    <ol class="relative mx-2 border-gray-200 dark:border-gray-700">
+                        <li class="mb-10 ms-4">
+                            <div
+                                class="absolute -start-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"
+                            ></div>
+                            <div class="mb-2">
+                                <x-status.chips :text="$historial->estado->nombre" class="mb-2" />
+                            </div>
+                            <div class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                                {{ \Carbon\Carbon::parse($historial->fecha_actualizacion)->format('d/m/Y') . '  ' . \Carbon\Carbon::parse($historial->hora_actualizacion)->format('h:i A') }}
+                            </div>
+                            @if ($historial->comentario)
+                                <h3 class="text-lg font-semibold text-red-900 dark:text-white">Comentarios</h3>
+                                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                                    {{ $historial->comentario }}
+                                </p>
+                            @endif
+        
+                            @if ($historial->foto_evidencia)
+                                <a
+                                    href="#"
+                                    class="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+                                >
+                                    Evidencia
+                                    <svg
+                                        class="ms-2 h-3 w-3 rtl:rotate-180"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 10"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                                        />
+                                    </svg>
+                                </a>
+                            @endif
+        
+                            <div class="block items-center p-3 hover:bg-gray-100 dark:hover:bg-gray-700 sm:flex">
+                                <img
+                                    class="mb-3 me-3 h-8 w-8 rounded-full sm:mb-0"
+                                    src="/storage/reportes/evidencia/uo274RP1t9Rth2VkWYatbMB8Qmz2oHkNzOkcqIkr.jpg"
+                                    alt="Jese Leos image"
+                                />
+                                <div class="text-gray-600 dark:text-gray-400">
+                                    @php
+                                        $persona = $historial->empleadoPuesto->usuario->persona;
+                                    @endphp
+                                    <div class="text-sm font-normal">{{ $persona->nombre . ' ' . $persona->apellido }}</div>
+                                </div>
+                            </div>
+                        </li>
+                    </ol>
+                @endforeach
             </div>
             <div class="w-full lg:w-[20%] px-8">
                 <button id="abrirActualizarSeguimiento"
