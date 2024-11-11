40e57d1954883cd2be28fab049725e8528954f7c
daiv05 <dc19019@ues.edu.sv>
2024-11-11 16:00:32 -0600

    validar mostrar bloque de lugar

--- resources/views/reportes/detail.blade.php
+++ resources/views/reportes/detail.blade.php
@@ -34,25 +34,26 @@
 
                     </div>
                 </div>
-                <div class="mb-4">
-                    <!-- Fila 3 -->
-                    <div class="font-semibold">
-                        <div class="flex flex-row gap-6">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="size-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
-                            </svg>
-                            Lugar
+                @if ($reporte->aula)
+                    <div class="mb-4">
+                        <!-- Fila 3 -->
+                        <div class="font-semibold">
+                            <div class="flex flex-row gap-6">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                    stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
+                                </svg>
+                                Lugar
+                            </div>
+                            <div class="ml-12 mt-2">
+                                <input type="text"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                                    placeholder="Aula de ejemplo" value="{{ $reporte->aula?->nombre }}" disabled/>
+                            </div>
                         </div>
-                        <div class="ml-12 mt-2">
-                            <input type="text"
-                                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-                                   placeholder="Aula de ejemplo" value="{{ $reporte->aula?->nombre }}" disabled/>
-                        </div>
-
                     </div>
-                </div>
+                @endif
                 @if (isset($reporte->actividad))
                     <div class="mb-4">
                         <div class="font-semibold">
@@ -62,7 +63,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                           d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"/>
                                 </svg>
-                                Actividad seleccionada
+                                Actividad reportada
                             </div>
                             <div class="ml-12 mt-2">
                                 <table class="text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
