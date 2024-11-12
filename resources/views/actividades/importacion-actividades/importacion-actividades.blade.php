192ae9ad11ac9a52ecc274265411999f87b0ddd6
MisaelGG11 <gg20031@ues.edu.sv>
2024-11-12 06:30:45 -0600

    feat: add validation to import activities

--- resources/views/actividades/importacion-actividades/importacion-actividades.blade.php
+++ resources/views/actividades/importacion-actividades/importacion-actividades.blade.php
@@ -9,45 +9,51 @@
 
     <div>
         <div class="max-w-7xl mx-auto sm:px-1 lg:px-3">
-
+            @php
+                $cicloActivo = \App\Models\Mantenimientos\Ciclo::with('tipoCiclo')->where('activo', 1)->first();
+            @endphp
             <div class="bg-white overflow-hidden shadow-md sm:rounded-lg p-6">
-
-                <div class="grid grid-cols-1">
-                    <div class="col-span-1">
-                        <h2 class="text-lg font-semibold text-gray-700">Instrucciones</h2>
-                        <p class="text-sm text-gray-600">Utiliza la plantilla de actividades, llena la información y sube el archivo.</p>
+                @if(isset($cicloActivo))
+                    <h1 class="text-xl font-bold text-orange-900 mb-2">Ciclo activo: {{ $cicloActivo->anio.'-'. $cicloActivo->tipoCiclo->nombre }}</h1>
+                    <div class="grid grid-cols-1">
+                        <div class="col-span-1">
+                            <h2 class="text-lg font-semibold text-gray-700">Instrucciones</h2>
+                            <p class="text-sm text-gray-600">Utiliza la plantilla de actividades, llena la información y sube el archivo.</p>
+                        </div>
+                        <div class="col-span-1">
+                            <form action="{{ route('importar-actividades-post') }}" method="POST" enctype="multipart/form-data" class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                @csrf
+                                <div class="mt-4">
+                                    <label for="tipo" class="block text-sm font-medium text-gray-700">Tipo de actividad</label>
+                                    <select id="tipo_actividad" name="tipo_actividad" class="mt-1 block
+                                        w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                        <option value="evento">Evento</option>
+                                        <option value="clase">Clase</option>
+                                    </select>
+                                </div>
+                                <div class="flex items-center justify-center w-full">
+                                    <label for="file" class="w-64 flex flex-col items-center px-4 py-6 bg-white text-orange-900 rounded-lg shadow-lg tracking-wide uppercase border border-orange-900 cursor-pointer hover:bg-orange-900 hover:text-white">
+                                        <x-heroicon-o-cloud-arrow-up class="w-10 h-10" />
+                                        <span id="file-name" class="mt-2 text-base leading-normal">
+                                            Selecciona un archivo
+                                        </span>
+                                        <input type="file" name="excel_file" id="file" class="hidden" onchange="updateFileName(this)" />
+                                        @if ($errors->has('excel_file'))
+                                            <span class="text-red-500 text-sm text-center">{{ $errors->first('excel_file') }}</span>
+                                        @endif
+                                    </label>
+                                </div>
+                                <div class="flex items-center justify-center w-full mt-4 md:col-span-2">
+                                    <button type="submit" class="ms-6 rounded-lg bg-red-700 px-8 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">
+                                        Subir archivo
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div class="col-span-1">
-                        <form action="{{ route('importar-actividades-post') }}" method="POST" enctype="multipart/form-data" class="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            @csrf
-                            <div class="mt-4">
-                                <label for="tipo" class="block text-sm font-medium text-gray-700">Tipo de actividad</label>
-                                <select id="tipo_actividad" name="tipo_actividad" class="mt-1 block
-                                    w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                                    <option value="evento">Evento</option>
-                                    <option value="clase">Clase</option>
-                                </select>
-                            </div>
-                            <div class="flex items-center justify-center w-full">
-                                <label for="file" class="w-64 flex flex-col items-center px-4 py-6 bg-white text-orange-900 rounded-lg shadow-lg tracking-wide uppercase border border-orange-900 cursor-pointer hover:bg-orange-900 hover:text-white">
-                                    <x-heroicon-o-cloud-arrow-up class="w-10 h-10" />
-                                    <span id="file-name" class="mt-2 text-base leading-normal">
-                                        Selecciona un archivo
-                                    </span>
-                                    <input type="file" name="excel_file" id="file" class="hidden" onchange="updateFileName(this)" />
-                                    @if ($errors->has('excel_file'))
-                                        <span class="text-red-500 text-sm text-center">{{ $errors->first('excel_file') }}</span>
-                                    @endif
-                                </label>
-                            </div>
-                            <div class="flex items-center justify-center w-full mt-4 md:col-span-2">
-                                <button type="submit" class="ms-6 rounded-lg bg-red-700 px-8 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">
-                                    Subir archivo
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                @else
+                    <h1 class="text-xl font-bold text-orange-900">No hay un ciclo activo</h1>
+                @endif
             </div>
 
             @if(session()->has('success'))
@@ -63,6 +69,7 @@
                     $tipoClases = \App\Models\General\TipoClase::all();
                     $modalidades = \App\Models\General\Modalidad::all();
                     $dias = \App\Models\General\Dia::all();
+                    $cicloActivo = \App\Models\General\Ciclo::where('activo', 1)->first();
                 @endphp
 
                 <h1 class="text-xl font-bold text-orange-900 mt-5 mb-3">Vista previa de la información</h1>
