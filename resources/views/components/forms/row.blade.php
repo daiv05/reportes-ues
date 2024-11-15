19474d98f4359524d22353e327a652868d79ebdc
MisaelGG11 <gg20031@ues.edu.sv>
2024-11-15 16:50:55 -0600

    feat: add required prop to inputs

--- resources/views/components/forms/row.blade.php
+++ resources/views/components/forms/row.blade.php
@@ -4,6 +4,6 @@
     'fullRow' => false,
 ])
 
-<div {{ $attributes->merge(['class' => $fullRow ? 'col-span-full mb-6' : "grid gap-6 md:grid-cols-$columns mb-6"]) }}>
+<div {{ $attributes->merge(['class' => $fullRow ? 'col-span-full mb-6' : "grid gap-6 md:grid-cols-$columns mb-4"]) }}>
     {{ $slot }}
 </div>
