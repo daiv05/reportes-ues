30767f8e5c019c6599d37bada8b578ecfb28128e
proyectosbme <bryanivan21165@gmail.com>
2024-11-17 14:23:37 -0600

    update dev

--- app/Http/Controllers/Auditorias/UserAuditController.php
+++ app/Http/Controllers/Auditorias/UserAuditController.php
@@ -5,30 +5,41 @@ use OwenIt\Auditing\Models\Audit;
 use App\Models\Seguridad\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class UserAuditController extends Controller
 {
        public function index(Request $request)
     {
         $query = Audit::query();
+        $filtersApplied = false;
 
         if ($request->has('model') && $request->model) {
             $query->where('auditable_type', $request->model);
+            $filtersApplied = true;
         }
 
         if ($request->has('event') && $request->event) {
             $query->where('event', $request->event);
+            $filtersApplied = true;
         }
 
         if ($request->has('user_id') && $request->user_id) {
             $query->where('user_id', $request->user_id);
+            $filtersApplied = true;
         }
 
         if ($request->has('start_date') && $request->has('end_date') && $request->start_date && $request->end_date) {
             $query->whereRaw('DATE(created_at) BETWEEN ? AND ?', [$request->start_date, $request->end_date]);
+            $filtersApplied = true;
         }
 
-        $audits = $query->paginate(5);
+
+        if (!$filtersApplied) {
+            $audits = new LengthAwarePaginator([], 0, 5);
+        } else {
+            $audits = $query->paginate(5);
+        }
 
         $models = Audit::select('auditable_type')->distinct()->get();
 
