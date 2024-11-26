226de8fb036967fae00823a9b2b8acc1df10f001
daiv05 <dc19019@ues.edu.sv>
2024-11-26 15:58:54 -0600

    vista forbidden + pruebas de middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,5 +45,15 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof UnauthorizedException) {
+            return response()->view("errors.forbidden", [
+                "exception" => $e
+            ], 403);
+        }
+        return parent::render($request, $e);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,10 @@ Route::get('/inicio', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/forbidden', function () {
+        return view('errors.forbidden');
+    })->name('errors.forbidden');
+
     Route::get('/perfil', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -63,7 +67,7 @@ Route::middleware('auth')->group(function () {
     /* ***************************************** */
     Route::prefix('reportes')->group(function () {
         // Reportes
-        Route::get('/listado-general', [ReporteController::class, 'index'])->name('reportes-generales');
+        Route::middleware('permission:REPORTES_VER_LISTADO_GENERAL')->get('/listado-general', [ReporteController::class, 'index'])->name('reportes-generales');
         Route::get('/registrar', [ReporteController::class, 'create'])->name('crear-reporte');
         Route::post('/store', [ReporteController::class, 'store'])->name('reportes.store');
         Route::get('/detalle/{id}', [ReporteController::class, 'detalle'])->name('detalle-reporte');
