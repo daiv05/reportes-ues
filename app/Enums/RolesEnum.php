9e0f4dc252ab34f870f6b6a4a53757b66a9dbea4
daiv05 <dc19019@ues.edu.sv>
2024-11-26 11:57:39 -0600

    permisos + validaciones en memu items

--- app/Enums/RolesEnum.php
+++ app/Enums/RolesEnum.php
@@ -9,4 +9,61 @@ enum RolesEnum: string
     case SUPERVISOR_REPORTE = 'ROLE_USUARIO_SUPERVISOR_REPORTE';
     case EMPLEADO = 'ROLE_USUARIO_EMPLEADO';
     case USUARIO = 'ROLE_USUARIO_BASE';
+
+    public function permisos(): array
+    {
+        $perm = [];
+        $rolePermissions = [
+            self::SUPERADMIN->value => array_map(fn($per) => $per->value, PermisosEnum::cases()),
+            self::ADMIN_REPORTE->value => [
+                PermisosEnum::REPORTES_CREAR->value,
+                PermisosEnum::REPORTES_ASIGNAR->value,
+                PermisosEnum::REPORTES_ACTUALIZAR_ESTADO->value,
+                PermisosEnum::REPORTES_VER_LISTADO_GENERAL->value,
+                PermisosEnum::REPORTES_VER_ASIGNACIONES->value,
+                PermisosEnum::REPORTES_REVISION_SOLUCION->value,
+                PermisosEnum::ACTIVIDADES_REALIZAR_REPORTE->value,
+                PermisosEnum::RECURSOS_VER->value,
+                PermisosEnum::RECURSOS_CREAR->value,
+                PermisosEnum::RECURSOS_EDITAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_VER->value,
+                PermisosEnum::UNIDADES_MEDIDA_CREAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_EDITAR->value
+            ],
+            self::SUPERVISOR_REPORTE->value => [
+                PermisosEnum::REPORTES_CREAR->value,
+                PermisosEnum::REPORTES_ACTUALIZAR_ESTADO->value,
+                PermisosEnum::REPORTES_VER_LISTADO_GENERAL->value,
+                PermisosEnum::REPORTES_VER_ASIGNACIONES->value,
+                PermisosEnum::REPORTES_REVISION_SOLUCION->value,
+                PermisosEnum::ACTIVIDADES_REALIZAR_REPORTE->value,
+                PermisosEnum::RECURSOS_VER->value,
+                PermisosEnum::RECURSOS_CREAR->value,
+                PermisosEnum::RECURSOS_EDITAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_VER->value,
+                PermisosEnum::UNIDADES_MEDIDA_CREAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_EDITAR->value
+            ],
+            self::EMPLEADO->value => [
+                PermisosEnum::REPORTES_CREAR->value,
+                PermisosEnum::REPORTES_ACTUALIZAR_ESTADO->value,
+                PermisosEnum::REPORTES_VER_LISTADO_GENERAL->value,
+                PermisosEnum::REPORTES_VER_ASIGNACIONES->value,
+                PermisosEnum::ACTIVIDADES_REALIZAR_REPORTE->value,
+                PermisosEnum::RECURSOS_VER->value,
+                PermisosEnum::RECURSOS_CREAR->value,
+                PermisosEnum::RECURSOS_EDITAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_VER->value,
+                PermisosEnum::UNIDADES_MEDIDA_CREAR->value,
+                PermisosEnum::UNIDADES_MEDIDA_EDITAR->value
+            ],
+            self::USUARIO->value => [
+                PermisosEnum::REPORTES_CREAR->value,
+                PermisosEnum::REPORTES_VER_LISTADO_GENERAL->value,
+                PermisosEnum::ACTIVIDADES_REALIZAR_REPORTE->value
+            ]
+        ];
+        $perm = $rolePermissions[$this->value] ?? [];
+        return $perm;
+    }
 }
