a56de71c766c1c4d41b447bc7e29c780cc9ef0fc
proyectosbme <bryanivan21165@gmail.com>
2024-11-27 21:30:00 -0600

    Cambios en user

--- app/Http/Controllers/Auditorias/UserAuditController.php
+++ app/Http/Controllers/Auditorias/UserAuditController.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Auditorias;
+
 use OwenIt\Auditing\Models\Audit;
 use App\Models\Seguridad\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Jenssegers\Agent\Agent;
+use App\Models\Registro\Persona;
 
 class UserAuditController extends Controller
 {
-       public function index(Request $request)
+    public function index(Request $request)
     {
         $query = Audit::query();
         $filtersApplied = false;
@@ -25,9 +27,18 @@ class UserAuditController extends Controller
             $filtersApplied = true;
         }
 
-        if ($request->has('user_id') && $request->user_id) {
-            $query->where('user_id', $request->user_id);
-            $filtersApplied = true;
+
+        if ($request->has('titulo') && $request->titulo) {
+            $personas = Persona::where('nombre', 'like', '%' . $request->titulo . '%')
+                ->orWhere('apellido', 'like', '%' . $request->titulo . '%')
+                ->pluck('id');
+
+            if ($personas->isNotEmpty()) {
+                $query->whereIn('user_id', $personas);
+                $filtersApplied = true;
+            } else {
+                $query->whereRaw('1 = 0');
+            }
         }
 
         if ($request->has('start_date') && $request->has('end_date') && $request->start_date && $request->end_date) {
--- app/Models/Seguridad/User.php
+++ app/Models/Seguridad/User.php
@@ -31,6 +31,7 @@ class User extends Authenticatable implements Auditable
         'id_persona',
         'activo',
         'id_escuela',
+        'es_estudiante', 
     ];
 
     protected $hidden = [
