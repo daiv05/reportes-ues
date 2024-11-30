6cdac480207ee0ff35a81df25891ca0484bcabdf
Leonardo Efigenio <83801094+leryunard@users.noreply.github.com>
2024-11-30 16:44:45 -0600

    fix(Reportes/Detalle/Actualizar): Validaciones a nivel de front añadidas.

--- resources/views/reportes/partials/modal-update-assignment.blade.php
+++ resources/views/reportes/partials/modal-update-assignment.blade.php
@@ -35,10 +35,11 @@
                 <span id="evidencia_error" class="text-red-500 text-sm"></span>
                 @include('components.forms.input-error', ['messages' => $errors->get('evidencia')])
             </div>
-            <div class="mt-4 w-full">
-                <label for="recursos_utilizados" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Recursos
-                    utilizados</label>
-                <select id="recursos_utilizados" multiple="" data-hs-select='{
+            @if(!($reporte->relacion_usuario['supervisor'] && $reporte->estado_ultimo_historial?->id === 4))
+                <div class="mt-4 w-full">
+                    <label for="recursos_utilizados" class="block text-sm font-medium text-gray-700 dark:text-gray-300">Recursos
+                        utilizados</label>
+                    <select id="recursos_utilizados" multiple="" data-hs-select='{
                           "hasSearch": true,
                           "isSearchDirectMatch": false,
                           "searchPlaceholder": "Búsqueda de recursos",
@@ -52,76 +53,77 @@
                           "optionTemplate": "<div class=\"flex items-center\"><div class=\"me-2\" data-icon></div><div><div class=\"hs-selected:font-semibold text-sm text-gray-800 \" data-title></div></div><div class=\"ms-auto\"><span class=\"hidden hs-selected:block\"><svg class=\"shrink-0 size-4 text-blue-600\" xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" viewBox=\"0 0 16 16\"><path d=\"M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z\"/></svg></span></div></div>",
                           "extraMarkup": "<div class=\"absolute top-1/2 end-3 -translate-y-1/2\"><svg class=\"shrink-0 size-3.5 text-gray-500 \" xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><path d=\"m7 15 5 5 5-5\"/><path d=\"m7 9 5-5 5 5\"/></svg></div>"
                         }' class="block" onchange="updateTable()">
-                    <option value="">Choose</option>
-                    @foreach ($recursos as $recurso)
-                        <option value="{{ $recurso->id }}">{{ $recurso->nombre }}</option>
-                    @endforeach
-                </select>
-                <input type="hidden" id="recursos_input" name="recursos">
-                <table id="recursos_table" class="mt-4 w-full">
-                    <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Cantidad</th>
-                        <th>Fondo</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <!-- Filas dinámicas se añadirán aquí -->
-                    </tbody>
-                </table>
-                <span id="recursos_utilizados_error" class="text-red-500 text-sm"></span>
-                @include('components.forms.input-error', ['messages' => $errors->get('recursos_utilizados')])
+                        <option value="">Choose</option>
+                        @foreach ($recursos as $recurso)
+                            <option value="{{ $recurso->id }}">{{ $recurso->nombre }}</option>
+                        @endforeach
+                    </select>
+                    <input type="hidden" id="recursos_input" name="recursos">
+                    <table id="recursos_table" class="mt-4 w-full">
+                        <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Cantidad</th>
+                            <th>Fondo</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <!-- Filas dinámicas se añadirán aquí -->
+                        </tbody>
+                    </table>
+                    <span id="recursos_utilizados_error" class="text-red-500 text-sm"></span>
+                    @include('components.forms.input-error', ['messages' => $errors->get('recursos_utilizados')])
 
-                <script>
-                    function updateTable() {
-                        const select = document.getElementById('recursos_utilizados');
-                        const tableBody = document.getElementById('recursos_table').querySelector('tbody');
-                        tableBody.innerHTML = ''; // Clear the table
+                    <script>
+                        function updateTable() {
+                            const select = document.getElementById('recursos_utilizados');
+                            const tableBody = document.getElementById('recursos_table').querySelector('tbody');
+                            tableBody.innerHTML = ''; // Clear the table
 
-                        Array.from(select.selectedOptions).forEach(option => {
-                            const row = document.createElement('tr');
-                            row.setAttribute('data-id', option.value);
+                            Array.from(select.selectedOptions).forEach(option => {
+                                const row = document.createElement('tr');
+                                row.setAttribute('data-id', option.value);
 
-                            row.innerHTML = `
+                                row.innerHTML = `
                                 <td>${option.text}</td>
                                 <td><input type="number" name="cantidad_${option.value}" class="border rounded p-1" min="1" value="1"></td>
                                 <td>
                                   <select name="fondo_${option.value}" class="border rounded p-1">
                                     @foreach ($fondos as $fondo)
-                            <option value="{{ $fondo->id }}">{{ $fondo->nombre }}</option>
+                                <option value="{{ $fondo->id }}">{{ $fondo->nombre }}</option>
                                     @endforeach
-                            </select>
-                          </td>
+                                </select>
+                              </td>
 `;
 
-                            tableBody.appendChild(row);
-                        });
-                    }
-
-                    function collectTableData() {
-                        const tableBody = document.getElementById('recursos_table').querySelector('tbody');
-                        const rows = tableBody.querySelectorAll('tr');
-                        const recursos = [];
-
-                        rows.forEach(row => {
-                            const id = row.getAttribute('data-id');
-                            const cantidad = row.querySelector(`input[name="cantidad_${id}"]`).value;
-                            const id_fondo = row.querySelector(`select[name="fondo_${id}"]`).value;
-
-                            recursos.push({
-                                id_recurso: id,
-                                cantidad: cantidad,
-                                id_fondo: id_fondo
+                                tableBody.appendChild(row);
                             });
-                        });
+                        }
 
-                        document.getElementById('recursos_input').value = JSON.stringify(recursos);
-                    }
+                        function collectTableData() {
+                            const tableBody = document.getElementById('recursos_table').querySelector('tbody');
+                            const rows = tableBody.querySelectorAll('tr');
+                            const recursos = [];
 
-                    document.querySelector('form').addEventListener('submit', collectTableData);
-                </script>
-            </div>
+                            rows.forEach(row => {
+                                const id = row.getAttribute('data-id');
+                                const cantidad = row.querySelector(`input[name="cantidad_${id}"]`).value;
+                                const id_fondo = row.querySelector(`select[name="fondo_${id}"]`).value;
+
+                                recursos.push({
+                                    id_recurso: id,
+                                    cantidad: cantidad,
+                                    id_fondo: id_fondo
+                                });
+                            });
+
+                            document.getElementById('recursos_input').value = JSON.stringify(recursos);
+                        }
+
+                        document.querySelector('form').addEventListener('submit', collectTableData);
+                    </script>
+                </div>
+            @endif
         </div>
         <div class="flex justify-center px-6 py-4 bg-gray-100 dark:bg-gray-800 text-right w-full">
             <button type="button" class="bg-gray-500 text-white px-4 py-2 rounded"
@@ -136,7 +138,7 @@
 </x-modal>
 
 <script>
-    document.getElementById('updateForm').addEventListener('submit', function(event) {
+    document.getElementById('updateForm').addEventListener('submit', function (event) {
         let valid = true;
 
         document.getElementById('id_estado_error').textContent = '';
