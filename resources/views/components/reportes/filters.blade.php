403b0c057bb754f5329e9fd0ba268110f706866d
daiv05 <dc19019@ues.edu.sv>
2025-01-14 14:36:04 -0600

    fixes responsive en app layout

--- resources/views/components/reportes/filters.blade.php
+++ resources/views/components/reportes/filters.blade.php
@@ -1,6 +1,16 @@
 
 @props(['ruta' => 'reportes-generales'])
 
+@php 
+    $objFiltro = [
+        'hoy' => 'Hoy',
+        '7_dias' => 'Últimos 7 días',
+        '30_dias' => 'Últimos 30 días',
+        'mes' => 'Último mes',
+        'anio' => 'Último año',
+    ];
+@endphp
+
 <div class="flex flex-col space-y-4 p-4 sm:flex-row sm:space-y-0 sm:space-x-4 overflow-y-visible">
     <form method="GET" action="{{ route($ruta) }}"
           class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
@@ -16,7 +26,7 @@
                     <path
                         d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z"/>
                 </svg>
-                {{ request('filter-radio') ? ucfirst(str_replace('_', ' ', request('filter-radio'))) : 'Filtrar' }}
+                {{ request('filter-radio') ? $objFiltro[request('filter-radio')] : 'Filtrar' }}
                 <svg class="ms-2.5 h-2.5 w-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                      fill="none" viewBox="0 0 10 6">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
