9da434eead0f53d6a4cebcc84d54568a706eb613
MisaelGG11 <gg20031@ues.edu.sv>
2025-01-21 15:55:38 -0600

    fix: max length and pattern for aulas

--- resources/views/mantenimientos/aulas/index.blade.php
+++ resources/views/mantenimientos/aulas/index.blade.php
@@ -11,18 +11,28 @@
         <x-header.simple titulo="Gestión de aulas" />
         <div class="p-6">
             @canany(['AULAS_CREAR'])
-            <x-forms.primary-button data-modal-target="static-modal" data-modal-toggle="static-modal" class="block"
-                type="button">
-                Añadir
-            </x-forms.primary-button>
+                <x-forms.primary-button
+                    data-modal-target="static-modal"
+                    data-modal-toggle="static-modal"
+                    class="block"
+                    type="button"
+                >
+                    Añadir
+                </x-forms.primary-button>
             @endcanany
         </div>
     </x-slot>
     <x-container>
         {{-- Filtros --}}
-        <div class="flex-col flex flex-wrap items-center justify-between space-y-4 pb-4 sm:flex-row sm:space-y-0 w-full">
-            <form action="{{ route('aulas.index') }}" method="GET" class="flex-row flex flex-wrap items-center space-x-8 mt-4 w-full">
-                <div class="flex w-full flex-col md:w-2/6 px-4 md:px-0">
+        <div
+            class="flex w-full flex-col flex-wrap items-center justify-between space-y-4 pb-4 sm:flex-row sm:space-y-0"
+        >
+            <form
+                action="{{ route('aulas.index') }}"
+                method="GET"
+                class="mt-4 flex w-full flex-row flex-wrap items-center space-x-8"
+            >
+                <div class="flex w-full flex-col px-4 md:w-2/6 md:px-0">
                     <x-forms.row :columns="1">
                         <x-forms.field
                             id="materia"
@@ -33,22 +43,40 @@
                     </x-forms.row>
                 </div>
                 <div class="flex flex-wrap space-x-4">
-                    <button type="submit"
-                            class="align-middle rounded-full inline-flex items-center px-3 py-3 border border-transparent shadow-sm text-sm font-medium text-white bg-escarlata-ues hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-4 w-4">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"/>
+                    <button
+                        type="submit"
+                        class="inline-flex items-center rounded-full border border-transparent bg-escarlata-ues px-3 py-3 align-middle text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-4 w-4"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                            />
                         </svg>
                     </button>
 
-                    <button type="reset"
-                            class="align-middle rounded-full inline-flex items-center px-3 py-3 shadow-sm text-sm font-medium bg-white border border-gray-500 text-gray-500 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-                            onclick="window.location.href='{{ route('aulas.index') }}';">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-4 w-4">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M6 18L18 6M6 6l12 12"/>
+                    <button
+                        type="reset"
+                        class="inline-flex items-center rounded-full border border-gray-500 bg-white px-3 py-3 align-middle text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                        onclick="window.location.href='{{ route('aulas.index') }}';"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="h-4 w-4"
+                        >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                 </div>
@@ -68,12 +96,16 @@
                         </x-table.td>
                         <x-table.td>
                             @canany(['AULAS_EDITAR'])
-                            <a href="#"
-                                class="edit-button font-medium text-green-600 hover:underline dark:text-green-400"
-                                data-id="{{ $aula->id }}" data-nombre="{{ $aula->nombre }}"
-                                data-facultad="{{ $aula->facultad->id }}" data-activo="{{ $aula->activo }}">
-                                <x-heroicon-o-pencil class="h-5 w-5" />
-                            </a>
+                                <a
+                                    href="#"
+                                    class="edit-button font-medium text-green-600 hover:underline dark:text-green-400"
+                                    data-id="{{ $aula->id }}"
+                                    data-nombre="{{ $aula->nombre }}"
+                                    data-facultad="{{ $aula->facultad->id }}"
+                                    data-activo="{{ $aula->activo }}"
+                                >
+                                    <x-heroicon-o-pencil class="h-5 w-5" />
+                                </a>
                             @endcanany
                         </x-table.td>
                     </x-table.tr>
@@ -81,11 +113,12 @@
             </x-table.base>
         </div>
 
-        <nav class="flex-column flex flex-wrap items-center justify-center pt-4 md:flex-row"
-            aria-label="Table navigation">
+        <nav
+            class="flex-column flex flex-wrap items-center justify-center pt-4 md:flex-row"
+            aria-label="Table navigation"
+        >
             {{ $aulas->links() }}
         </nav>
-
     </x-container>
     <x-form-modal id="static-modal">
         <x-slot name="header">
@@ -96,41 +129,71 @@
                 @csrf
                 <div id="general-errors" class="mb-4 text-sm text-red-500"></div>
                 <x-forms.row :columns="1">
-                    <x-forms.select label="Facultad" id="id_facultad" name="id_facultad" :options="$facultades->pluck('nombre', 'id')->toArray()"
-                        :value="old('id_facultad')" :error="$errors->get('id_facultad')" required />
+                    <x-forms.select
+                        label="Facultad"
+                        id="id_facultad"
+                        name="id_facultad"
+                        :options="$facultades->pluck('nombre', 'id')->toArray()"
+                        :value="old('id_facultad')"
+                        :error="$errors->get('id_facultad')"
+                        required
+                    />
                     <div id="facultad-error" class="text-sm text-red-500"></div>
                 </x-forms.row>
                 <x-forms.row :columns="2">
                     <div>
-                        <x-forms.field id="nombre" label="Nombre" name="nombre" :value="old('nombre')" :error="$errors->get('nombre')"
-                            required />
+                        <x-forms.field
+                            id="nombre"
+                            label="Nombre"
+                            name="nombre"
+                            pattern="^[a-zA-Z0-9]{1,30}$"
+                            patternMessage="Solo se permiten 30 caracteres que sean letras o números"
+                            :value="old('nombre')"
+                            :error="$errors->get('nombre')"
+                            required
+                        />
                         <div id="nombre-error" class="text-sm text-red-500"></div>
                     </div>
                     <div>
-                        <x-forms.select label="Estado" id="activo" name="activo" :options="['1' => 'ACTIVO', '0' => 'INACTIVO']"
-                            :value="old('activo', '1')" :error="$errors->get('activo')" required />
+                        <x-forms.select
+                            label="Estado"
+                            id="activo"
+                            name="activo"
+                            :options="['1' => 'ACTIVO', '0' => 'INACTIVO']"
+                            :value="old('activo', '1')"
+                            :error="$errors->get('activo')"
+                            required
+                        />
                         <div id="estado-error" class="text-sm text-red-500"></div>
                     </div>
                 </x-forms.row>
             </form>
         </x-slot>
         <x-slot name="footer">
-            <button data-modal-hide="static-modal" type="button"
-                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:z-10 focus:outline-none focus:ring-4">
+            <button
+                data-modal-hide="static-modal"
+                type="button"
+                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:z-10 focus:outline-none focus:ring-4"
+            >
                 Cancelar
             </button>
-            <button type="submit" form="asignacion-form"
-                class="ms-6 rounded-lg bg-red-700 px-8 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4">
+            <button
+                type="submit"
+                form="asignacion-form"
+                class="ms-6 rounded-lg bg-red-700 px-8 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+            >
                 Guardar
             </button>
         </x-slot>
     </x-form-modal>
 </x-app-layout>
 <script>
-    document.getElementById('asignacion-form').addEventListener('submit', function(event) {
+    document.getElementById('asignacion-form').addEventListener('submit', function (event) {
         const nombreField = document.getElementById('nombre');
         const facultadField = document.getElementById('id_facultad');
         const estadoField = document.getElementById('activo');
+
+        const patternErrors = document.querySelectorAll('div[id*="pattern-error"]');
 
         let hasErrors = false;
 
@@ -156,24 +219,27 @@
             event.preventDefault();
             document.getElementById('general-errors').innerHTML = 'Todos los campos son requeridos';
         }
+
+        if (patternErrors.length > 0) {
+            event.preventDefault();
+        }
     });
 
     document.querySelectorAll('[data-modal-hide="static-modal"]').forEach((button) => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             updateModalTitle('Añadir Aula');
 
             resetForm();
 
             document.getElementById('asignacion-form').method = 'POST';
-            document.getElementById('asignacion-form').action = "{{ route('aulas.store') }}";
+            document.getElementById('asignacion-form').action = '{{ route('aulas.store') }}';
 
             document.querySelectorAll('input[name="_method"]').forEach((input) => input.remove());
-
         });
     });
 
     document.querySelectorAll('.edit-button').forEach((button) => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             const id = this.getAttribute('data-id');
             const nombre = this.getAttribute('data-nombre');
             const facultad = this.getAttribute('data-facultad');
@@ -193,11 +259,9 @@
             document.getElementById('id_facultad').value = facultad;
             document.getElementById('activo').value = activo;
 
-
             document.querySelector('[data-modal-target="static-modal"]').click();
         });
     });
-
 
     function updateModalTitle(title) {
         document.getElementById('modal-title').textContent = title;
@@ -207,9 +271,7 @@
         document.getElementById('asignacion-form').reset();
         document.getElementById('general-errors').innerHTML = '';
 
-        document.querySelectorAll('.text-red-500').forEach((error) => (error.innerHTML =
-            ''));
-
+        document.querySelectorAll('.text-red-500').forEach((error) => (error.innerHTML = ''));
 
         document.querySelectorAll('select').forEach((select) => {
             select.selectedIndex = 0;
