307e3d1bf7ec7b40c46cca7244c7272f66759b22
daiv05 <dc19019@ues.edu.sv>
2025-01-22 10:48:03 -0600

    add suport for pwa v1.0

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @laravelPWA
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -29,7 +29,7 @@
                 <div class="mx-auto max-w-[95%] lg:px-2 mt-3">
                     <div class="overflow-hidden bg-white py-4 shadow-sm dark:bg-gray-800 sm:rounded-lg">
                         <div class="p-3 text-center text-xl font-bold text-red-900 dark:text-gray-100">
-                            404 Not Found
+                            Offline
                         </div>
                     </div>
                 </div>
@@ -40,16 +40,16 @@
                         <div class="overflow-auto">
                             <div class="container mx-auto mt-12 px-4">
                                 <div class="text-center mb-12">
-                                    <h2 class="text-4xl font-bold mb-4 text-orange-900">No se ha encontrado el recurso
+                                    <h2 class="text-4xl font-bold mb-4 text-orange-900">Actualmente no tienes conexión a
+                                        internet
                                     </h2>
-                                    <p class="text-xl text-gray-600">Lo sentimos, pero el recurso que buscas no se
-                                        existe o no se
-                                        encuentra disponible,</p>
-                                    <p class="text-xl text-gray-600 mb-16">por favor verifica la URL e intenta de nuevo
+                                    <p class="text-xl text-gray-600 mb-8">
+                                        Por favor verifica tu conexión a internet e intenta de nuevo
                                     </p>
                                     <a href="/"
-                                        class="bg-orange-900 text-white px-4 py-2 rounded-lg mt-4 hover:bg-orange-800">Volver
-                                        a la página principal</a>
+                                        class="bg-orange-900 text-white px-4 py-2 rounded-lg mt-4 hover:bg-orange-800">
+                                        Recargar
+                                    </a>
                                 </div>
                             </div>
                         </div>
