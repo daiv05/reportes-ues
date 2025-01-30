5ae597621fe8685898d754c9fbf0b34ef67b05c2
daiv05 <dc19019@ues.edu.sv>
2025-01-30 09:28:01 -0600

    se agrega atributo nombre_unaccent para busqueda por coincidencia

--- app/Models/Mantenimientos/Recurso.php
+++ app/Models/Mantenimientos/Recurso.php
@@ -6,6 +6,7 @@ use App\Models\Reportes\RecursoReporte;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Normalizer;
 use OwenIt\Auditing\Contracts\Auditable;
 
 class Recurso extends Model implements Auditable
@@ -14,13 +15,22 @@ class Recurso extends Model implements Auditable
 
     protected $table = 'recursos';
 
+    protected $appends = ['nombre_unaccent'];
+
     protected $fillable = [
         'nombre',
         'activo',
     ];
+
     public function setNombreAttribute($value)
     {
         $this->attributes['nombre'] = strtoupper(strtr($value, 'áéíóú', 'ÁÉÍÓÚ'));
+    }
+
+    public function getNombreUnaccentAttribute()
+    {
+        $nombre_normalizado = Normalizer::normalize($this->nombre, Normalizer::FORM_D);
+        return preg_replace('/\pM/u', '', $nombre_normalizado);
     }
 
     public function recursosReportes() : HasMany
