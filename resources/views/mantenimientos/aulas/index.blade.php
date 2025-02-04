62903f8b6cc192012c40e458fd2dbbdd2a2e502c
MisaelGG11 <gg20031@ues.edu.sv>
2025-02-04 09:28:30 -0600

    feat: add tootltips for maintenances

--- resources/views/mantenimientos/aulas/index.blade.php
+++ resources/views/mantenimientos/aulas/index.blade.php
@@ -45,6 +45,7 @@
                 <div class="flex flex-wrap space-x-4">
                     <button
                         type="submit"
+                        data-tooltip-target="tooltip-aplicar-filtros"
                         class="inline-flex items-center rounded-full border border-transparent bg-escarlata-ues px-3 py-3 align-middle text-sm font-medium text-white shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
                     >
                         <svg
@@ -63,10 +64,20 @@
                         </svg>
                     </button>
 
+                    <div
+                        id="tooltip-aplicar-filtros"
+                        role="tooltip"
+                        class="shadow-xs tooltip z-40 inline-block rounded-lg bg-escarlata-ues px-3 py-2 text-sm font-medium text-white opacity-0 transition-opacity duration-300 dark:bg-gray-700"
+                    >
+                        Aplicar filtros
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+
                     <button
                         type="reset"
                         class="inline-flex items-center rounded-full border border-gray-500 bg-white px-3 py-3 align-middle text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
                         onclick="window.location.href='{{ route('aulas.index') }}';"
+                        data-tooltip-target="tooltip-limpiar-filtros"
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -79,6 +90,15 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
+
+                    <div
+                        id="tooltip-limpiar-filtros"
+                        role="tooltip"
+                        class="shadow-xs tooltip z-40 inline-block rounded-lg bg-gray-200 px-3 py-2 text-sm font-medium text-escarlata-ues opacity-0 transition-opacity duration-300 dark:bg-gray-700"
+                    >
+                        Limpiar filtros
+                        <div class="tooltip-arrow" data-popper-arrow></div>
+                    </div>
                 </div>
             </form>
         </div>
