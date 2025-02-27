38e86dd8875341ba8cab4a198f5c3c26e686a92f
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2025-02-27 08:51:13 -0600

    cambios

--- resources/views/audits/index.blade.php
+++ resources/views/audits/index.blade.php
@@ -27,57 +27,34 @@
                 <x-forms.primary-button class="ml-3">Filtrar</x-forms.primary-button>
             </x-forms.button-group>
             <x-forms.row :columns="3">
-                <x-forms.select
-                    label="Filtrar por Modelo"
-                    id="model"
-                    name="model"
-                    :options="$models->mapWithKeys(
-                        fn($model) => [$model->auditable_type => class_basename($model->auditable_type)],
-                    )"
-                    :selected="request('model')"
-                    :error="$errors->get('model')"
-                />
+                <x-forms.select label="Filtrar por Modelo" id="model" name="model" :options="$models->mapWithKeys(
+                    fn($model) => [$model->auditable_type => class_basename($model->auditable_type)],
+                )"
+                    :selected="request('model')" :error="$errors->get('model')" />
                 <div>
-                    <x-forms.select
-                        label="Filtrar por Acción"
-                        id="event"
-                        name="event"
-                        :options="collect($events)->mapWithKeys(
-                            fn($event) => [
-                                $event['event'] => ucfirst($event['event']),
-                            ],
-                        )"
-                        :value="old('event')"
-                    />
+                    <x-forms.select label="Filtrar por Acción" id="event" name="event" :options="collect($events)->mapWithKeys(
+                        fn($event) => [
+                            $event['event'] => ucfirst($event['event']),
+                        ],
+                    )"
+                        :value="old('event')" />
                 </div>
 
                 <div>
                     <x-forms.input-label for="titulo" :value="__('Nombre')" />
                     <div class="relative">
                         <div
-                            class="rtl:inset-r-0 pointer-events-none absolute inset-y-0 left-0 flex items-center ps-3 rtl:right-0"
-                        >
-                            <svg
-                                class="h-4 w-5 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"                            >
-                                <path
-                                    fill-rule="evenodd"
+                            class="rtl:inset-r-0 pointer-events-none absolute inset-y-0 left-0 flex items-center ps-3 rtl:right-0">
+                            <svg class="h-4 w-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
                                     d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    clip-rule="evenodd"
-                                ></path>
+                                    clip-rule="evenodd"></path>
                             </svg>
                         </div>
-                        <input
-                            type="text"
-                            id="table-search"
-                            name="titulo"
-                            value="{{ request('titulo') }}"
+                        <input type="text" id="table-search" name="titulo" value="{{ request('titulo') }}"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2 ps-10 text-sm text-gray-900 focus:border-red-500 focus:ring-red-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-red-500 dark:focus:ring-red-500 sm:w-80"
-                            placeholder="Buscar por nombre"
-                        />
+                            placeholder="Buscar por nombre" />
                     </div>
                     <x-forms.input-error :messages="$errors->get('titulo')" class="mt-2" />
                 </div>
@@ -87,26 +64,16 @@
                     <x-forms.input-label for="start_date" :value="__('Fecha Inicial')" />
                     <div class="relative max-w-sm">
                         <div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
-                            <svg
-                                class="h-4 w-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 20 20">
+                            <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path
-                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                                />
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <input
-                            datepicker
-                            name="start_date"
-                            id="start_date"
-                            type="text"
-                            value="start_date"
+                        <input datepicker name="start_date" id="start_date" type="text"
+                            :value="request('start_date')"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-red-500 focus:ring-red-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-red-500 dark:focus:ring-red-500"
-                            placeholder="Seleccione una fecha"
-                        />
+                            placeholder="Seleccione una fecha" />
                     </div>
                     <x-forms.input-error :messages="$errors->get('start_date')" class="mt-2" />
                 </div>
@@ -115,27 +82,15 @@
                     <x-forms.input-label for="end_date" :value="__('Fecha Final')" />
                     <div class="relative max-w-sm">
                         <div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3.5">
-                            <svg
-                                class="h-4 w-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                            >
+                            <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                 <path
-                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"
-                                />
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
                             </svg>
                         </div>
-                        <input
-                            datepicker
-                            name="end_date"
-                            id="end_date"
-                            type="text"
-                            value="end_date"
+                        <input datepicker name="end_date" id="end_date" type="text" :value="request('end_date')"
                             class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-red-500 focus:ring-red-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-red-500 dark:focus:ring-red-500"
-                            placeholder="Seleccione una fecha"
-                        />
+                            placeholder="Seleccione una fecha" />
                     </div>
                     <x-forms.input-error :messages="$errors->get('end_date')" class="mt-2" />
                 </div>
@@ -156,26 +111,23 @@
                                 $user = \App\Models\Seguridad\User::find($audi->user_id);
                             @endphp
 
-                            @if (! $user)
+                            @if (!$user)
                                 @php
                                     $user = \App\Models\Seguridad\User::find($audi->auditable_id);
                                 @endphp
                             @endif
 
                             @if ($user)
-                                    {{ $user->persona->nombre }} {{ $user->persona->apellido }}
+                                {{ $user->persona->nombre }} {{ $user->persona->apellido }}
                             @endif
                         </x-table.td>
                         <x-table.td>{{ $audi->event }}</x-table.td>
                         <x-table.td justify="center">{{ class_basename($audi->auditable_type) }}</x-table.td>
                         <x-table.td justify="center">{{ $audi->auditable_id }}</x-table.td>
                         <x-table.td justify="center">
-                            @if ($audi->new_values && ! empty($audi->new_values))
-                                <button
-                                    data-modal-target="modalNuevo-{{ $audi->id }}"
-                                    data-modal-toggle="modalNuevo-{{ $audi->id }}"
-                                    type="button"
-                                >
+                            @if ($audi->new_values && !empty($audi->new_values))
+                                <button data-modal-target="modalNuevo-{{ $audi->id }}"
+                                    data-modal-toggle="modalNuevo-{{ $audi->id }}" type="button">
                                     <x-heroicon-o-eye class="h-5 w-5" />
                                 </button>
                             @else
@@ -183,12 +135,9 @@
                             @endif
                         </x-table.td>
                         <x-table.td justify="center">
-                            @if ($audi->old_values && ! empty($audi->old_values))
-                                <button
-                                    data-modal-target="modalViejo-{{ $audi->id }}"
-                                    data-modal-toggle="modalViejo-{{ $audi->id }}"
-                                    type="button"
-                                >
+                            @if ($audi->old_values && !empty($audi->old_values))
+                                <button data-modal-target="modalViejo-{{ $audi->id }}"
+                                    data-modal-toggle="modalViejo-{{ $audi->id }}" type="button">
                                     <x-heroicon-o-eye class="h-5 w-5" />
                                 </button>
                             @else
@@ -221,7 +170,7 @@
         @foreach ($audits as $audi)
             <!-- Modal para Datos Nuevos -->
             @isset($audi->new_values)
-                @if (! empty($audi->new_values))
+                @if (!empty($audi->new_values))
                     <x-form-modal id="modalNuevo-{{ $audi->id }}" class="hidden">
                         <x-slot name="header">
                             <h3 class="text-2xl font-bold text-escarlata-ues">Datos Nuevos</h3>
@@ -230,11 +179,8 @@
                             <pre>{{ json_encode($audi->new_values, JSON_PRETTY_PRINT) }}</pre>
                         </x-slot>
                         <x-slot name="footer">
-                            <button
-                                data-modal-hide="modalNuevo-{{ $audi->id }}"
-                                type="button"
-                                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:outline-none"
-                            >
+                            <button data-modal-hide="modalNuevo-{{ $audi->id }}" type="button"
+                                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:outline-none">
                                 Cerrar
                             </button>
                         </x-slot>
@@ -244,7 +190,7 @@
 
             <!-- Modal para Datos Viejos -->
             @isset($audi->old_values)
-                @if (! empty($audi->old_values))
+                @if (!empty($audi->old_values))
                     <x-form-modal id="modalViejo-{{ $audi->id }}" class="hidden">
                         <x-slot name="header">
                             <h3 class="text-2xl font-bold text-escarlata-ues">Datos Viejos</h3>
@@ -253,11 +199,8 @@
                             <pre>{{ json_encode($audi->old_values, JSON_PRETTY_PRINT) }}</pre>
                         </x-slot>
                         <x-slot name="footer">
-                            <button
-                                data-modal-hide="modalViejo-{{ $audi->id }}"
-                                type="button"
-                                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:outline-none"
-                            >
+                            <button data-modal-hide="modalViejo-{{ $audi->id }}" type="button"
+                                class="rounded-lg border bg-gray-700 px-7 py-2.5 text-sm font-medium text-white focus:outline-none">
                                 Cerrar
                             </button>
                         </x-slot>
@@ -268,7 +211,7 @@
     </x-container>
 
     <script>
-        document.getElementById('model').addEventListener('change', function () {
+        document.getElementById('model').addEventListener('change', function() {
             var model = this.value;
             if (model) {
                 fetch(`/bitacora/get-events?model=${model}`)
@@ -279,7 +222,8 @@
                         data.forEach((event) => {
                             var option = document.createElement('option');
                             option.value = event.event;
-                            option.textContent = event.event.charAt(0).toUpperCase() + event.event.slice(1);
+                            option.textContent = event.event.charAt(0).toUpperCase() + event.event
+                                .slice(1);
                             eventSelect.appendChild(option);
                         });
                     });
@@ -291,7 +235,7 @@
 </x-app-layout>
 <script>
     document.querySelectorAll('[data-modal-toggle]').forEach((button) => {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             const modalId = this.getAttribute('data-modal-target');
             const modal = document.getElementById(modalId);
             if (modal) {
@@ -301,7 +245,7 @@
     });
 
     document.querySelectorAll('[data-modal-hide]').forEach((button) => {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             const modalId = this.getAttribute('data-modal-hide');
             const modal = document.getElementById(modalId);
             if (modal) {
@@ -312,13 +256,11 @@
 </script>
 
 <script>
+    flatpickr("#start_date", {
+        dateFormat: "d/m/Y", // Ensure the date format matches the PHP parsing
+    });
 
-flatpickr("#start_date", {
-    dateFormat: "d/m/Y",  // Ensure the date format matches the PHP parsing
-});
-
-flatpickr("#end_date", {
-    dateFormat: "d/m/Y",  // Ensure the date format matches the PHP parsing
-});
-
+    flatpickr("#end_date", {
+        dateFormat: "d/m/Y", // Ensure the date format matches the PHP parsing
+    });
 </script>
