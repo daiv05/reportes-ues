6c07595639f4d131649aab6c271a3d3c14f702ed
Ivan Marroquin <bryan.marroquin@coop1.com.sv>
2025-02-28 07:40:43 -0600

    etsandarizar pantallas y cambiar filtros

--- app/Http/Controllers/rhu/EntidadesController.php
+++ app/Http/Controllers/rhu/EntidadesController.php
@@ -16,38 +16,31 @@ class EntidadesController extends Controller
     public function index(): View
     {
         $nombreFilter = request('nombre-filter');
-
-        // entidades paginados
+        
         $entidades = Entidades::when($nombreFilter, function ($query, $nombreFilter) {
             return $query->where('nombre', 'like', "%$nombreFilter%");
         })
             ->where('activo', true)
             ->paginate(GeneralEnum::PAGINACION->value)->appends(request()->query());
-
-        // Lista jerárquica de entidades para el select
         $entidadesLista = $this->getHierarchicalEntidades();
-        //dd($entidades);
+
 
         return view('rhu.entidades.index', compact('entidades', 'entidadesLista'));
     }
     private function getHierarchicalEntidades($parentId = null, $parentName = '')
     {
-        // Obtener entidades cuyo id_entidad (padre) coincide con $parentId
+
         $entidades = Entidades::where('id_entidad', $parentId)
             ->where('activo', true)
-            ->orderBy('id') // Ordenar para mantener el orden adecuado
+            ->orderBy('id')
             ->get();
-
-        $result = collect(); // Utilizar una colección para facilitar el manejo de los datos
+        $result = collect();
         foreach ($entidades as $entidad) {
-            // Construir el nombre jerárquico completo del departamento
             $nombreCompleto = $parentName ? $parentName . ' --> ' . $entidad->nombre : $entidad->nombre;
 
-            // Actualizar el nombre del departamento con el nombre completo
             $entidad->nombre = $nombreCompleto;
             $result->push($entidad);
 
-            // Añadir los hijos de manera recursiva, acumulando los nombres de los padres
             $nuevoParentName = $nombreCompleto;
             $result = $result->merge($this->getHierarchicalEntidades($entidad->id, $nuevoParentName));
         }
@@ -57,7 +50,6 @@ class EntidadesController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        // Validación de los campos del formulario
         $validatedData = $request->validate([
             'nombre' => 'required|unique:entidades|max:50|regex:/^[a-zA-Z0-9.ñÑáéíóúÁÉÍÓÚüÜ\s]+$/',
             'descripcion' => 'required|max:250',
@@ -71,18 +63,13 @@ class EntidadesController extends Controller
             'id_entidad.exists' => 'La entidad padre seleccionado no existe',
         ]);
 
-        // Determinar la jerarquía según el departamento padre
         if (empty($validatedData['id_entidad'])) {
-            $validatedData['jerarquia'] = 0; // Si no hay departamento padre, es un departamento raíz con jerarquía 0
+            $validatedData['jerarquia'] = 0;
         } else {
-            // Obtener el departamento padre para calcular la jerarquía
             $departamentoPadre = Entidades::find($validatedData['id_entidad']);
             $validatedData['jerarquia'] = $departamentoPadre->jerarquia + 1;
         }
-
         Entidades::create($validatedData);
-
-        // Redirección a la vista de departamentos con un mensaje de éxito (opcional)
         return redirect()->route('entidades.index')->with('message', [
             'type' => 'success',
             'content' => 'Entidad creada exitosamente'
@@ -94,7 +81,7 @@ class EntidadesController extends Controller
     {
         $request->validate(
             [
-                'nombre' => ['required', 'regex:/^[a-zA-Z0-9.ñÑáéíóúÁÉÍÓÚüÜ\s]+$/',  Rule::unique('entidades')->ignore($id), 'max:50'],
+                'nombre' => ['required', 'regex:/^[a-zA-Z0-9.ñÑáéíóúÁÉÍÓÚüÜ\s]+$/', Rule::unique('entidades')->ignore($id), 'max:50'],
                 'descripcion' => 'required|max:250',
                 'activo' => 'required|boolean',
                 'id_entidad' => 'nullable|exists:entidades,id|not_in:' . $id,
@@ -109,11 +96,9 @@ class EntidadesController extends Controller
             ]
         );
         $entidad = Entidades::findOrFail($id);
-        // Ajustar jerarquía y entidad padre si se selecciona uno
         if ($request->filled('id_entidad')) {
             $entidadPadre = Entidades::findOrFail($request->input('id_entidad'));
-            // Verificar si se está generando un ciclo
-            if ($this->esDescendiente($entidadPadre->id, $entidad->id)) {
+                    if ($this->esDescendiente($entidadPadre->id, $entidad->id)) {
                 Session::flash('message', [
                     'type' => 'warning',
                     'content' => 'No puedes seleccionar un descendiente como padre'
@@ -152,7 +137,7 @@ class EntidadesController extends Controller
         return redirect()->route('entidades.index');
     }
 
-    public function esDescendiente($idNuevoPadre, $idEntidad) : bool
+    public function esDescendiente($idNuevoPadre, $idEntidad): bool
     {
         $padre = Entidades::findOrFail($idNuevoPadre);
 
